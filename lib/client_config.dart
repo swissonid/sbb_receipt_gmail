9dcdb28a8003e04e5443be342f35e87dd9fa4d47
Patrice Müller <mueller.patrice@gmail.com>
2022-07-15 09:37:36 +0200

    Added client config without any information

---
+++ lib/client_config.dart
@@ -0,0 +1,5 @@
+import 'package:googleapis_auth/auth_io.dart';
+
+// Please consult this guide for how to create OAuth ClientId
+// https://developers.google.com/workspace/guides/create-credentials#oauth-client-id
+final clientIt = ClientId('YOUR IDENTIFIER', 'YOUR CLIENT SECRET');
